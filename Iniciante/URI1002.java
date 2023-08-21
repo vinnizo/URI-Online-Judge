e742b1181fbb1a5dacf4008d25df9a03d2b76cff
Vinícius <142107226+vinnizo@users.noreply.github.com>
2023-08-21 15:33:57 -0300

    Create URI1002.java

---
+++ Iniciante/URI1002.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args){
+		Scanner leitor = new Scanner(System.in);
+		double raio = leitor.nextDouble();
+		double area = 3.14159 * (raio * raio);
+		System.out.println(String.format("A=%.4f", area)); 
+	}
+}
